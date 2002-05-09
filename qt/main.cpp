1222dfb0c9107fef2c8b2bb749127d0e5e1eb4c2
Steffen Hansen <hansen@kde.org>
2002-05-09 14:38:03 +0000

    Fall back to curses if DISPLAY is not set

--- qt/main.cpp
+++ qt/main.cpp
@@ -17,6 +17,11 @@
    Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
    02111-1307, USA  */
 
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+
 extern "C"
 {
 #include "memory.h"
@@ -37,11 +42,39 @@ extern "C"
 #include "pinentrydialog.h"
 #include "pinentrycontroller.h"
 
+
+
+#ifdef FALLBACK_CURSES
+#include <pinentry.h>
+#include <pinentry-curses.h>
+
+pinentry_cmd_handler_t pinentry_cmd_handler = curses_cmd_handler;
+
+int curses_main (int argc, char *argv[])
+{
+  pinentry_init ();
+
+  /* Consumes all arguments.  */
+  if (pinentry_parse_opts (argc, argv))
+    {
+      printf ("pinentry-curses " VERSION "\n");
+      exit (EXIT_SUCCESS);
+    }
+
+  if (pinentry_loop ())
+    return 1;
+
+  return 0;
+}
+#endif
+
 extern "C++" {
   extern bool is_secure;
 };
 
+#ifndef VERSION
 #define VERSION "0.1"
+#endif
 
 #ifdef USE_KDE
 static const char *description =
@@ -62,30 +95,44 @@ void my_new_handler()
   qFatal("Out of memory!");
 }
 
-int main( int argc, char** argv ) 
+int qt_main( int argc, char *argv[] )
 {
-  secmem_init( 16384*8 );
-  secmem_set_flags(SECMEM_WARN);
-  drop_privs();
-  set_new_handler(my_new_handler);
-  try {
+    secmem_init( 16384*8 );
+    secmem_set_flags(SECMEM_WARN);
+    drop_privs();
+    set_new_handler(my_new_handler);
+    try {
 #ifdef USE_KDE
-    KAboutData aboutData( "pinentry", I18N_NOOP("Pinentry"),
-			  VERSION, description, KAboutData::License_GPL,
-			  "(c) 2001, Steffen Hansen, Klarälvdalens Datakonsult AB", 0, 0, "klaralvdalens-datakonsult.se");
-    aboutData.addAuthor("Steffen Hansen, Klarälvdalens Datakonsult AB",0, "steffen@klaralvdalens-datakonsult.se");
-    KCmdLineArgs::init( argc, argv, &aboutData );
-    KCmdLineArgs::addCmdLineOptions( options ); // Add our own options.
-    KApplication app;
+      KAboutData aboutData( "pinentry", I18N_NOOP("Pinentry"),
+			    VERSION, description, KAboutData::License_GPL,
+			    "(c) 2001, Steffen Hansen, Klarälvdalens Datakonsult AB", 0, 0, "klaralvdalens-datakonsult.se");
+      aboutData.addAuthor("Steffen Hansen, Klarälvdalens Datakonsult AB",0, "steffen@klaralvdalens-datakonsult.se");
+      KCmdLineArgs::init( argc, argv, &aboutData );
+      KCmdLineArgs::addCmdLineOptions( options ); // Add our own options.
+      KApplication app;
 #else
-    QApplication app( argc, argv );
+      QApplication app( argc, argv );
 #endif // USE_KDE
-    is_secure = true;
+      is_secure = true;
+      
+      PinEntryController ctrl;
+      ctrl.exec();
+      return 0;
+    } catch( std::bad_alloc& ex ) {
+      qDebug("Out of memory, got a %s", ex.what());
+      return -1;
+    }
+}
 
-    PinEntryController ctrl;
-    ctrl.exec();
-    return 0;
-  } catch( std::bad_alloc& ex ) {
-    qDebug("Out of memory, got a %s", ex.what());
+int main( int argc, char* argv[] ) 
+{
+#ifdef FALLBACK_CURSES
+  if( getenv( "DISPLAY" ) ) {
+#endif
+    return qt_main( argc, argv );
+#ifdef FALLBACK_CURSES
+  } else {
+    return curses_main( argc, argv );
   }
+#endif
 }
