a1e48a2e1076e98c7c6be7c4ce89faaab5d59b10
Mento <Mento@gpgtools.org>
2017-04-26 14:39:28 +0200

    Use keychain by default.

--- macosx/AppDelegate.m
+++ macosx/AppDelegate.m
@@ -34,6 +34,7 @@ pinentry_cmd_handler_t pinentry_cmd_handler = mac_cmd_handler;
 
 - (void)applicationDidFinishLaunching:(NSNotification *)notification {
 	[[NSUserDefaults standardUserDefaults] addSuiteNamed:@"org.gpgtools.common"];
+	[[NSUserDefaults standardUserDefaults] registerDefaults:@{@"UseKeychain": @YES}];
 
 	if (pinentry_loop()) {
 		exit(1);
